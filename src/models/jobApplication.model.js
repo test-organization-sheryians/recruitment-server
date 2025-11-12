ada9c4d37d78ce898aeba395fa88d4e236e18976
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-12 16:09:39 +0530

    model created

---
+++ src/models/jobApplication.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const jobApplicationSchema = new mongoose.Schema({
+            jobId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "Job",
+                        required: true,
+            },
+            candidateId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: "User",
+                        required: true
+            },
+            resumeUrl: String,
+            message: String,
+            status: {
+                        type: String,
+                        enum: ["applied", "shortlisted", "rejected", "forwareded", "interview", "hired"],
+                        default: "pending",
+            },
+            appliedAt: {
+                        type: Date,
+                        default: Date.now,
+            },
+}, { timestamps: true });
+
+// Prevent same user from applying twice for same job
+jobApplicationSchema.index({ jobId: 1, userId: 1 }, { unique: true });
+
+export default mongoose.model("JobApplication", jobApplicationSchema);
