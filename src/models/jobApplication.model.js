c11370fed1a677cfa2b33014e73b1a1f399c40cb
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-13 19:08:00 +0530

    fixed the model

--- src/models/jobApplication.model.js
+++ src/models/jobApplication.model.js
@@ -1,37 +1,37 @@
 import mongoose from "mongoose";
 
 const jobApplicationSchema = new mongoose.Schema(
-  {
-    jobId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "JobRole",
-      required: true,
-    },
-    candidateId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    resumeUrl: String,
-    message: String,
-    status: {
-      type: String,
-      enum: [
-        "applied",
-        "shortlisted",
-        "rejected",
-        "forwareded",
-        "interview",
-        "hired",
-      ],
-      default: "pending",
-    },
-    appliedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { timestamps: true }
+            {
+                        jobId: {
+                                    type: mongoose.Schema.Types.ObjectId,
+                                    ref: "JobRole",
+                                    required: true,
+                        },
+                        candidateId: {
+                                    type: mongoose.Schema.Types.ObjectId,
+                                    ref: "User",
+                                    required: true,
+                        },
+                        resumeUrl: String,
+                        message: String,
+                        status: {
+                                    type: String,
+                                    enum: [
+                                                "applied",
+                                                "shortlisted",
+                                                "rejected",
+                                                "forwareded",
+                                                "interview",
+                                                "hired",
+                                    ],
+                                    default: "applied",
+                        },
+                        appliedAt: {
+                                    type: Date,
+                                    default: Date.now,
+                        },
+            },
+            { timestamps: true }
 );
 
 // Prevent same user from applying twice for same job
