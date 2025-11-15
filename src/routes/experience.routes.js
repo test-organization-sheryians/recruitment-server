3f9770e31f6e08538c0c8212e3c9b0005e64b9a4
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-15 19:52:34 +0530

    added validation middleware

--- src/routes/experience.routes.js
+++ src/routes/experience.routes.js
@@ -1,22 +1,24 @@
 import express from "express";
-// import ExperienceController from "../controllers/experience.controller";
 import ExperienceController from "../controllers/experience.controller.js";
+import { experienceCreateValidator, experienceUpdateSchema } from "../middlewares/validators/experience.validator.js";
+import validateRequest from "../middlewares/validators/validateRequest.js";
 
 const router = express.Router();
 const expereniceController = new ExperienceController();
 
+
 // create a new experience
-router.post("/", expereniceController.createExperience)
+router.post("/", validateRequest(experienceCreateValidator), expereniceController.createExperience)
 
 // get all experience for a candidate
 router.get("/:candidateId", expereniceController.getCandidateExperiences)
 
-// get a single experience by ID
 
+// get a single experience by ID
 router.get("/single/:id", expereniceController.getSingleExperience)
 
 // update experience
-router.patch("/:id", expereniceController.updateExperience)
+router.patch("/:id", validateRequest(experienceUpdateSchema), expereniceController.updateExperience)
 
 // Delete experience
 router.delete("/:id", expereniceController.deleteExperience)
