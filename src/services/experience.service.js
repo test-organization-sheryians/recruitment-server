0df9c525dbaeb94c428181cebf4314a73e01bdb3
Nitesh bakhla <niteshbakhla007@gmail.com>
2025-11-21 16:58:00 +0530

    feat: add ownership check for update and view experience

--- src/services/experience.service.js
+++ src/services/experience.service.js
@@ -15,21 +15,25 @@ class ExperienceService {
     return await this.experienceRepository.createExperience(data);
   }
 
-  async getCandidateExperiences(candidateId) {
+  async getCandidateExperiences(candidateId, userId) {
     if (!candidateId) {
       throw new AppError("candidateId is required", 400);
     }
+
     return await this.experienceRepository.findByCandidateId(
       candidateId
     );
   }
-  async getSingleExperience(id) {
+  async getSingleExperience(id, userId) {
     if (!id) {
       throw new AppError("experience id is required", 400);
     }
 
     const experience = await this.experienceRepository.getExperienceById(id);
-
+    if (experience.candidateId.toString() !== userId) {
+      throw new AppError("you are not allowed", 401);
+    }
+    console.log(id)
     if (!experience) {
       throw new AppError("Experience not found", 404);
     }
@@ -37,13 +41,20 @@ class ExperienceService {
     return experience;
   }
 
-  async updateExperience(experienceId, data) {
+  async updateExperience(experienceId, data, userId) {
 
     if (!experienceId) {
       throw new AppError("experience id is required", 400);
     }
     if (data.isCurrent) {
       data.endDate = null;
+    }
+
+
+    const user = await this.experienceRepository.getExperienceById(experienceId);
+
+    if (user.candidateId.toString() !== userId) {
+      throw new AppError("you are not allowed to updated", 401);
     }
     const updated = await this.experienceRepository.updateExperience(
       experienceId,
@@ -63,7 +74,7 @@ class ExperienceService {
     }
 
     const user = await this.experienceRepository.getExperienceById(experienceId);
- 
+
     if (userId !== user.candidateId.toString()) {
       throw new AppError("You are not allowed to delete", 401);
     }
