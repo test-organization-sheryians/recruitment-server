86fb030fa16cd9ab01f20f5288345b1b57f48fbb
kavita <yadavkavi2000@gmail.com>
2026-06-11 13:44:57 +0530

    Task completed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -21,6 +21,7 @@ export const authenticateJWT = async (req, res, next) => {
 
     const decoded = authService.verifyToken(token);
     console.log(decoded);
+
     if (!decoded.isVerified || decoded.isVerified === false) {
       throw new AppError("User is not verified", 401);
     }
